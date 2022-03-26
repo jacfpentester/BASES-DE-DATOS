7c61b03a142118ba99c4cdbb3b9d7923cfabc40d
Jose Antonio Cervantes <85256588+jacfpentester@users.noreply.github.com>
2022-03-26 21:58:43 +0100

    Create sentenciadelete.sql

---
+++ mysql/Ejercicios/sentenciadelete.sql
@@ -0,0 +1,28 @@
+/* ¡IMPORTANTE! Hay que estar MUY seguro de lo que se está haciendo en este proceso. Si
+borras un registro después no lo podrás recuperar si no hay backup.
+Por este motivo, o se hace en un entorno de pruebas o mediante transacciones SQL que se
+pueden deshacer */
+
+-- Usaremos la BD jardinería.
+use jardineria;
+
+-- 1 Borra los clientes que no tengan pedidos.
+
+-- 2 Borra los pagos del cliente con menor límite de crédito.
+
+-- 3 Borra los pagos por cheque.
+
+-- 4 Borra los pedidos y su detalle del cliente “Tendo Garden”.
+
+
+-- Usaremos la BD NBA.
+use nba;
+
+-- 5 Borra las estadísticas de los jugadores sin procedencia.
+
+-- 6 Borra los jugadores españoles.
+
+-- 7 Borra los equipos de las ciudades que tienen una o en la segunda posición.
+
+-- 8 Borra todo lo relacionado con el equipo 76ers en la base de datos.
+
